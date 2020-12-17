1f3695763dc4a00689c38373e7446776faa402ec
Felipe <felipepcan@gmail.com>
2020-12-17 14:57:40 -0300

    adicionando até capítulo 24 da seção 3

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,21 @@
+// Comentário de uma linha
+console.log('Linha 1')
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+*Comentário de
+*múltiplas linhas
+*/
+console.log('Linha 3')
+
+/*
+Dicas:
+1- Evite comentários "idiotas" = Repetição de algo já apresentado
+2- Procure ter nomes que sejam claros
+3- Comente tudo o que você acha necessário(só pro curso)
+*/
---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,22 @@
+var a = 3
+let b = 4
+/* 
+Melhor criar variáveis usando let
+*/
+var a = 30
+b = 40
+/*
+"var" da pra atribuir outro valor em outra linha de código
+já o "let" não.
+
+*/
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5
+// c = 50
+console.log(c)
